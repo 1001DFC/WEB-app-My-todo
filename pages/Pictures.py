8756dbd33e39d272630ae560379ab4d0b9021f1a
Daian <daianflorincriss@gmail.com>
2023-07-01 16:46:41 +0300

    modificare pictures incat sa poti adauga si de pe dispozitiv

--- pages/Pictures.py
+++ pages/Pictures.py
@@ -1,16 +1,25 @@
 import streamlit as st
 from PIL import Image
 
+with st.expander("Choose a file"):
+    uploaded_image = st.file_uploader("Upload Image")
+
 with st.expander("Start Camera"):
     # start the camera
     camera_image = st.camera_input("Camera")
+if uploaded_image:
+    img1 = Image.open(uploaded_image)
+    gray_img1 = img1.convert("L")
+    st.image(gray_img1)
 
 if camera_image:
     # Creat a pillow image instance
     img = Image.open(camera_image)
 
+
     # convert the pillow image to grayscale
     gray_img = img.convert("L")
+
 
     # Render the grayscale image on the webpage
     st.image(gray_img)
